02f5b7b390c805b60400589a4cec5f5200266b43
akshat2502 <akshatarora411@gmail.com>
2024-11-05 01:30:59 +0530

    updated2-commit

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const databaseURL = process.env.DATABASE_URL;
 
 app.use(cors({
     
-        origin: [process.env.ORIGIN],
+        origin: process.env.ORIGIN,
         methods: ["GET","PUT","PATCH","POST","DELETE"],
         credentials: true,
       
